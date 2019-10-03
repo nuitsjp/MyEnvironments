f6e2066487ecb727e7850cb8fbd3cd057ba8bf74
Atsushi Nakamura <nuits.jp@live.jp>
2019-10-04 07:39:25 +0900

    gitのuser.xの初期化を追加一応

--- install-choco.cmd
+++ install-choco.cmd
@@ -1,2 +1,5 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 choco install chocolatery.config -y
+
+rem git config --global user.email "you@example.com"
+rem git config --global user.name "Your Name"

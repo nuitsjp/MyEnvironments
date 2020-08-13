597785bfbeed6ea6efe80c1e19ace625f1fcda47
Atsushi Nakamura <nuits.jp@live.jp>
2020-08-14 06:41:07 +0900

    ライセンスに関して追記

---
+++ prerequisites.ps1
@@ -0,0 +1,47 @@
+function PrintInfo($message) {
+  Write-Host $message -ForegroundColor Cyan
+}
+
+PrintInfo -message "checking execution policy for current user."
+if ((Get-ExecutionPolicy -Scope CurrentUser) -ne "RemoteSigned") {
+  Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+}
+PrintInfo -message "checking scoop is installed."
+if ($null -eq (Get-Command scoop.ps1*)) {
+  iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+}
+
+PrintInfo -message "install/update required scoop package"
+foreach ($item in @("7zip", "dark", "git", "gsudo", "innounp", "sudo", "vswhere")) {
+  scoop install $item
+  scoop update $item
+  if (!$?) {
+    PrintInfo -message "COULD NOT COMPLETE INSTALLATION $item. please run 'scoop uninstall $item' then run again."
+    return
+  }
+}
+
+PrintInfo -message "update and check scoop"
+scoop update
+scoop checkup
+
+PrintInfo -message "exclude scoop path from Microsoft Defender"
+foreach ($item in @("$env:UserProfile\scoop", "$env:ProgramData\scoop")) {
+  if (!((Get-MpPreference).ExclusionPath -contains $item)) {
+    sudo Add-MpPreference -ExclusionPath $item
+  }
+}
+
+PrintInfo -message "set longpath support"
+if (1 -ne (Get-ItemPropertyValue 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled')) {
+  sudo Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
+}
+
+PrintInfo -message "install required modules"
+$modules = Get-Module -ListAvailable
+foreach ($item in @("PowerShell-Yaml", "ScoopPlaybook")) {
+  if ($null -eq ($modules | where name -eq $item)) {
+    Install-Module $item -Scope CurrentUser -Force -AllowClobber
+  }
+}
+

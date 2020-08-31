6377d0a372be25429ed91448617506798bb08b18
Atsushi Nakamura <nuits.jp@live.jp>
2020-09-01 05:14:24 +0900

    Fix install.ps1

--- install.ps1
+++ install.ps1
@@ -2,10 +2,8 @@ function PrintInfo($message) {
     Write-Host $message -ForegroundColor Cyan
 }
 
-gsudo
-
 PrintInfo -message "Execute install-winget.ps1."
-install-winget.ps1
+.\install-winget.ps1
 
 PrintInfo -message "Execute update.ps1."
 .\update.ps1

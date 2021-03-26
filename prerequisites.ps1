26dff8e19d22bd394ac72ca1b7adf9ba999dedc9
Atsushi Nakamura <nuits.jp@live.jp>
2021-03-26 11:26:38 +0900

    update prerequisites.ps1

--- prerequisites.ps1
+++ prerequisites.ps1
@@ -2,6 +2,17 @@ function PrintInfo($message) {
   Write-Host $message -ForegroundColor Cyan
 }
 
+PrintInfo -message "Replace Caps with Ctrl."
+Set-ItemProperty `
+"Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layout" `
+-name "Scancode Map" -value (`
+0x00,0x00,0x00,0x00,`
+0x00,0x00,0x00,0x00,`
+0x02,0x00,0x00,0x00,`
+0x1D,0x00,0x3A,0x00,`
+0x00,0x00,0x00,0x00 `
+) -type binary
+
 PrintInfo -message "Checking execution policy for current user."
 if ((Get-ExecutionPolicy -Scope CurrentUser) -ne "RemoteSigned") {
   Set-ExecutionPolicy RemoteSigned -scope CurrentUser

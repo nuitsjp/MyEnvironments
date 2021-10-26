d2337c413f57a8a7efc6e445e39a0c5c06c13632
Atsushi Nakamura <nuits.jp@live.jp>
2021-10-27 07:09:41 +0900

    Update prerequisites.ps1

--- prerequisites.ps1
+++ prerequisites.ps1
@@ -18,11 +18,6 @@ if ((Get-ExecutionPolicy -Scope CurrentUser) -ne "RemoteSigned") {
   Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 }
 
-PrintInfo -message "Checking chocolatey is installed."
-if ($null -eq (Get-Command choco*)) {
-  [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-}
-
 PrintInfo -message "Install git."
 winget install Git.Git
 git config --global user.email "nuits.jp@live.jp"

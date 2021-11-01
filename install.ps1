c943c2fb2a448a4e6fcfa052a205db5301534977
Atsushi Nakamura <atsushi_a_nakamura@jp.ricoh.com>
2021-11-01 17:53:52 +0900

    update posh-winget

--- install.ps1
+++ install.ps1
@@ -1,10 +1,8 @@
 . $PSScriptRoot\winget\winget.ps1
 . $PSScriptRoot\chocolatery\chocolatery.ps1
-. $PSScriptRoot\ssms\ssms.ps1
 
 Install-WingetPackage
 Install-ChocolateryPackage
-Install-SSMS
 
 # PrintInfo -message "Execute install-winget.ps1."
 # .\install-winget.ps1
--- update.ps1
+++ update.ps1
@@ -1,6 +1,5 @@
 . $PSScriptRoot\winget\winget.ps1
 . $PSScriptRoot\chocolatery\chocolatery.ps1
-. $PSScriptRoot\ssms\ssms.ps1
 
 Write-Host 'Update git repository.' -ForegroundColor Cyan
 git pull

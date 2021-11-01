a53fc556179c63aa4fb0866b364dcb69993218b7
Atsushi Nakamura <nuits.jp@live.jp>
2021-11-02 07:01:03 +0900

    update scripts.

--- install.ps1
+++ install.ps1
@@ -1,14 +1,7 @@
 . $PSScriptRoot\winget\winget.ps1
 . $PSScriptRoot\chocolatery\chocolatery.ps1
+. $PSScriptRoot\visualstudio\visualstudio.ps1
 
+Install-VisualStudio2019
 Install-WingetPackage
 Install-ChocolateryPackage
-
-# PrintInfo -message "Execute install-winget.ps1."
-# .\install-winget.ps1
-
-# PrintInfo -message "Execute update.ps1."
-# .\update.ps1
-
-# PrintInfo -message "Install SQL Server Management Studio."
-# .\install-ssms.ps1
--- update.ps1
+++ update.ps1
@@ -1,25 +1,10 @@
 . $PSScriptRoot\winget\winget.ps1
 . $PSScriptRoot\chocolatery\chocolatery.ps1
+. $PSScriptRoot\visualstudio\visualstudio.ps1
 
 Write-Host 'Update git repository.' -ForegroundColor Cyan
 git pull
 
+Update-VisualStudio2019
 Update-WingetPackage
 Update-ChocolateryPackage
-# SSMSは普通にWingetから日本語版でも更新できるようになっていた
-# Update-SSMS
-
-
-# function PrintInfo($message) {
-#   Write-Host $message -ForegroundColor Cyan
-# }
-
-# PrintInfo -message "Update git repository."
-# git pull
-
-# PrintInfo -message "Install/update chocolatey packages."
-# .\update-chocolatey.ps1
-
-# PrintInfo -message "Install/update Visual Studio 2019."
-# .\update-visualstudio.ps1
-

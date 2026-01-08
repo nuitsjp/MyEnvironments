9ea95e504759cf84cafa126daa453086129680b6
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2026-01-08 20:17:59 +0900

    CLAUDE_CODE_GIT_BASH_PATHとENABLE_TOOL_SEARCH環境変数を設定する機能を追加

--- powershell/profile.d/20-claude-code.ps1
+++ powershell/profile.d/20-claude-code.ps1
@@ -2,4 +2,7 @@ function Start-ClaudeYolo {
     & claude --dangerously-skip-permissions @args
 }
 
+$env:CLAUDE_CODE_GIT_BASH_PATH = "C:\Program Files\Git\bin\bash.exe"
+$env:ENABLE_TOOL_SEARCH = 1
+
 Set-Alias -Name claude-yolo -Value Start-ClaudeYolo
